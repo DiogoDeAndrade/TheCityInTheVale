bdc4ed2b6b5a1ab6bb193569b08cab48ba65f644
Diogo Andrade <diogo.g.andrade@gmail.com>
2019-12-06 00:44:48 +0000

    Added terrain to game

---
+++ Assets/Scripts/Editor_PlaceOnTerrain.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+[ExecuteInEditMode]
+public class Editor_PlaceOnTerrain : MonoBehaviour
+{
+    public bool     rotateToSurface;
+
+    void Start()
+    {
+#if !UNITY_EDITOR
+        Destroy(this)
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (UnityEngine.Application.isPlaying) return;
+
+        List<RaycastHit> hits = new List<RaycastHit>(Physics.RaycastAll(new Ray(transform.position + Vector3.up * 10.0f, Vector3.down), 50.0f));
+
+        hits.Sort((x, y) => (x.distance < y.distance)?(-1):((x.distance == y.distance)?(0):(1)));
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject == gameObject) continue;
+            if (hit.collider.transform.IsChildOf(transform)) continue;
+
+            transform.position = hit.point;
+            if (rotateToSurface) transform.up = hit.normal;
+
+            break;
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/TerrainGen.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class TerrainGen : MonoBehaviour
+{
+    public int      resolution = 513;
+    public float    freqX = 1.0f;
+    public float    freqY = 1.0f;
+    public Vector3  size = new Vector3(500.0f, 100.0f, 500.0f);
+    public float    minHeight = 0.0f;
+    public int      heightRes = 0;
+
+    Terrain terrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [Button("Generate Terrain")]
+    void GenTerrain()
+    { 
+        terrain = GetComponent<Terrain>();
+
+        var terrainData = terrain.terrainData;
+
+        terrainData.heightmapResolution = resolution;
+        terrainData.size = size;
+
+        float[,] heights = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
+        float    h;
+
+        for (int y = 0; y < terrainData.heightmapResolution; y++)
+        {
+            for (int x = 0; x < terrainData.heightmapResolution; x++)
+            {
+                h = Mathf.PerlinNoise((float)x * freqX / (float)terrainData.heightmapResolution, (float)y * freqY / (float)terrainData.heightmapResolution);
+
+                if (h < minHeight) h = minHeight;
+
+                h = (h - minHeight) / (1.0f - minHeight);
+
+                if (heightRes > 0)
+                {
+                    h = Mathf.Floor(h * heightRes) / heightRes;
+                }
+
+                heights[x, y] = h;
+            }
+        }
+
+
+        terrainData.SetHeights(0, 0, heights);        
+
+        terrain.terrainData = terrainData;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
